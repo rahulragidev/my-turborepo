511a20bc7a3362efcd1aef1b7e2895ecf963aed3
rahulragidev <iamrahulragi@gmail.com>
2024-07-18 15:22:05 +0530

    tiptap & utils added

---
+++ packages/tiptap-custom-extensions/src/inline-page-link.tsx
@@ -0,0 +1,106 @@
+import { Node as TipTapNode, NodeViewRendererProps } from "@tiptap/core"
+import { NodeViewWrapper, ReactNodeViewRenderer } from "@tiptap/react"
+import { FeatherIcon } from "lucide-react"
+import Link from "next/link"
+
+export interface PageReferenceOptions {
+	href: string
+	name: string
+	HTMLAttributes: Record<string, any>
+}
+
+// declare module "@tiptap/core" {
+// 	interface Commands<ReturnType> {
+// 		inlinePageLink: {
+// 			/**
+// 			 * Set a highlight mark
+// 			 */
+// 			addPageLink: (attributes?: {
+// 				href: string
+// 				name: string
+// 				isPublic: boolean
+// 			}) => ReturnType
+// 		}
+// 	}
+// }
+
+const InlinePageLinkNodeView = (props: NodeViewRendererProps) => {
+	const { editor } = props
+	// @ts-expect-error
+	const siteId = editor.options?.editorProps?.attributes?.siteId as string
+	return (
+		<NodeViewWrapper className="inline-flex items-center space-x-2 w-fit px-2 py-1 bg-blue-200 hover:bg-blue-300 text-blue-800 rounded-md transition-none hover:shadow-md">
+			<Link href={`/${siteId}/${props.node.attrs.href}`}>
+				{props.node.attrs.name}
+			</Link>
+			{!props.node.attrs.isPublic && <FeatherIcon height={14} />}
+		</NodeViewWrapper>
+	)
+}
+
+const InlinePageLink = TipTapNode.create<PageReferenceOptions>({
+	name: "inline-page-link",
+
+	group: "inline",
+
+	inline: true,
+
+	selectable: false,
+
+	atom: true,
+
+	addAttributes() {
+		return {
+			href: {
+				default: null,
+			},
+			name: {
+				default: null,
+			},
+			HTMLAttributes: {
+				class: "text-blue-800 bg-blue-100 p-1 rounded-md",
+			},
+			isPublished: {
+				default: false,
+			},
+		}
+	},
+
+	parseHTML() {
+		return [
+			{
+				tag: `a[data-type="inline-page-reference-mark"]`,
+			},
+		]
+	},
+
+	renderHTML({ HTMLAttributes }) {
+		return [
+			"a",
+			{
+				"data-type": "inline-page-reference-mark",
+				...HTMLAttributes,
+			},
+			0,
+		]
+	},
+
+	addCommands() {
+		return {
+			addPageLink:
+				attributes =>
+				({ commands }) => {
+					return commands.insertContent({
+						type: this.name,
+						attrs: attributes,
+					})
+				},
+		}
+	},
+
+	addNodeView() {
+		return ReactNodeViewRenderer(InlinePageLinkNodeView)
+	},
+})
+
+export default InlinePageLink
---
+++ packages/tiptap-custom-extensions/src/page-node.tsx
@@ -0,0 +1,159 @@
+import {
+	Node as TipTapNode,
+	NodeViewWrapper,
+	NodeViewWrapperProps,
+	ReactNodeViewRenderer,
+	mergeAttributes,
+} from "@tiptap/react"
+import clsx from "clsx"
+import Button from "@repo/ui/button"
+import { cn } from "@repo/utils/cn"
+import Link from "next/link"
+import { ArrowRight, Trash } from "react-feather"
+
+type SetPageOptions = {
+	slug: string
+	id: string
+	name: string
+	description?: string
+	featured?: string
+	featuredimage?: string
+}
+
+declare module "@tiptap/core" {
+	// eslint-disable-next-line
+	interface Commands<ReturnType> {
+		page: {
+			/**
+			 * Insert a Page node
+			 */
+			// eslint-disable-next-line no-unused-vars
+			setPage: (options: SetPageOptions) => ReturnType
+		}
+	}
+}
+
+export default TipTapNode.create({
+	name: "page-node",
+	inline: false,
+	group: "block",
+	draggable: true,
+	atom: true,
+	// isolating: true,
+	defining: true,
+	gapCursor: true,
+	addAttributes() {
+		return {
+			slug: {
+				default: null,
+				parseHTML: element => element.getAttribute("data-slug"),
+				renderHTML: attributes => {
+					return {
+						"data-slug": attributes.slug,
+					}
+				},
+			},
+			id: {
+				default: null,
+				parseHTML: element => element.getAttribute("data-id"),
+				renderHTML: attributes => {
+					return {
+						"data-id": attributes.id,
+					}
+				},
+			},
+			name: {
+				default: null,
+			},
+			featured: {
+				default: null,
+			},
+			featuredImage: {
+				default: null,
+			},
+		}
+	},
+
+	parseHTML() {
+		return [
+			{
+				tag: 'div[data-type="page-node"]',
+			},
+		]
+	},
+
+	renderHTML({ HTMLAttributes }) {
+		return ["div", mergeAttributes(HTMLAttributes, { "data-type": "page-node" }), 0]
+	},
+	// Prevent delete or backspace from deleting the node
+	addKeyboardShortcuts() {
+		const isFeatured = this.editor.getAttributes("page-node").featured
+		return {
+			Backspace: () => {
+				if (this.editor.isActive("page-node") && !isFeatured) {
+					return true
+				}
+				return false
+			},
+			Delete: () => {
+				// This prevents the node from being deleted when the user presses backspace
+				if (this.editor.isActive("page-node") && !isFeatured) {
+					return true
+				}
+				return false
+			},
+		}
+	},
+
+	addCommands() {
+		return {
+			setPage:
+				(options: SetPageOptions) =>
+				({ commands }) => {
+					console.log("setPage options:", options)
+					return commands.insertContent({
+						type: "page-node",
+						attrs: options,
+					})
+				},
+		}
+	},
+
+	addNodeView() {
+		return ReactNodeViewRenderer(PageNodeView)
+	},
+})
+
+export const PageNodeView = (props: NodeViewWrapperProps) => {
+	const { node, selected, editor } = props
+	console.debug("PageNodeView Props:", props)
+	const siteId = editor.options.editorProps.attributes.siteId as string
+
+	return (
+		<NodeViewWrapper data-type="page-node" contentEditable={false}>
+			<div
+				contentEditable={false}
+				className={cn(
+					"bg-slate-950 rounded-md shadow flex items-center justify-between",
+					selected && "border-2 border-slate-600"
+				)}>
+				<Link
+					className={clsx(
+						"flex items-center pl-2 pr-2 hover:pl-2 w-[50vw] py-2 bg-transparpent rounded text-white ring-0 hover:opacity-90 hover:bg-slate-60"
+					)}
+					id={node.attrs.id}
+					href={`/${siteId}/${node.attrs.slug}`}
+					{...node.attrs}>
+					<span>{node.attrs.name || node.attrs.slug}</span>
+					<ArrowRight height="14" />
+				</Link>
+				<Button
+					variant="tertiary"
+					// eslint-disable-next-line no-alert
+					onClick={() => alert(`Delete page with id: ${node.attrs.id}`)}>
+					<Trash />
+				</Button>
+			</div>
+		</NodeViewWrapper>
+	)
+}
