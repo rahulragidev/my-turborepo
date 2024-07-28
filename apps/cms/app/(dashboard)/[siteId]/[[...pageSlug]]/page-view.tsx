d1561c1ef9ce2cce44d6020f672058782f58409f
rahulragidev <iamrahulragi@gmail.com>
2024-07-29 03:39:54 +0530

    build working

--- apps/cms/app/(dashboard)/[siteId]/[[...pageSlug]]/page-view.tsx
+++ apps/cms/app/(dashboard)/[siteId]/[[...pageSlug]]/page-view.tsx
@@ -8,7 +8,7 @@ import usePageBySlug from "dataHooks/usePage"
 import { motion } from "framer-motion"
 import { gql } from "graphql-request"
 import { GraphQLResponse } from "graphql-request/build/esm/types"
-import { cn } from "lib/utils"
+import { cn } from "@repo/utils/src/cn"
 import fetchWithToken from "libs/fetchers/fetchWithToken"
 import { useCallback, useEffect } from "react"
 import toast from "react-hot-toast"
@@ -29,141 +29,143 @@ import PageToolBar from "./components/page-tool-bar"
 // }
 
 const UPDATE_PAGE_MUTATION = gql`
-	mutation updatePages($data: UpdatePageInput!, $id: String!) {
-		data: updatePage(data: $data, id: $id) {
-			success
-			message
-			data {
-				name
-				id
-				body
-				isPublic
-				body
-				draftBody
-				jsonBody
-				jsonDraftBody
-				createdAt
-				updatedAt
-				lastPublishedAt
-			}
-		}
-	}
+    mutation updatePages($data: UpdatePageInput!, $id: String!) {
+        data: updatePage(data: $data, id: $id) {
+            success
+            message
+            data {
+                name
+                id
+                body
+                isPublic
+                body
+                draftBody
+                jsonBody
+                jsonDraftBody
+                createdAt
+                updatedAt
+                lastPublishedAt
+            }
+        }
+    }
 `
 
 const PageView = ({
-	token,
-	page,
-	userId,
-	siteId,
-	pageSlug,
+    token,
+    page,
+    userId,
+    siteId,
+    pageSlug
 }: {
-	token: string
-	page: Page
-	userId: string
-	siteId: string
-	pageSlug: string
+    token: string
+    page: Page
+    userId: string
+    siteId: string
+    pageSlug: string
 }) => {
-	const { mutate: pageMutate } = usePageBySlug({})
+    const { mutate: pageMutate } = usePageBySlug({})
 
-	const { isMinimized } = useMinimizeMaximize()
-	useEffect(() => {
-		// this will set the token inside upload-images.ts tiptap custom plugin.
-		setImageUploadToken(token)
-	}, [token])
+    const { isMinimized } = useMinimizeMaximize()
+    useEffect(() => {
+        // this will set the token inside upload-images.ts tiptap custom plugin.
+        setImageUploadToken(token)
+    }, [token])
 
-	const updatePage = useCallback(
-		async ({
-			draftBody,
-			jsonDraftBody,
-		}: {
-			draftBody?: string
-			jsonDraftBody: JSONContent
-		}): Promise<GraphQLResponse<PageResponse>> => {
-			// console.log("Update page called")
-			try {
-				const payload: UpdatePageInput = {
-					draftBody,
-					jsonDraftBody,
-				}
+    const updatePage = useCallback(
+        async ({
+            draftBody,
+            jsonDraftBody
+        }: {
+            draftBody?: string
+            jsonDraftBody: JSONContent
+        }): Promise<GraphQLResponse<PageResponse>> => {
+            // console.log("Update page called")
+            try {
+                const payload: UpdatePageInput = {
+                    draftBody,
+                    jsonDraftBody
+                }
 
-				const response = await fetchWithToken<GraphQLResponse<PageResponse>>({
-					query: UPDATE_PAGE_MUTATION,
-					variables: {
-						id: page.id,
-						data: payload,
-					},
-					token,
-				})
-				pageMutate()
-				return response
-			} catch (error: any) {
-				console.log("Errror updating page", error)
-				toast.error(error.message || "Error updating page")
-				return error
-			}
-		},
-		[token, page.id, pageMutate]
-	)
+                const response = await fetchWithToken<
+                    GraphQLResponse<PageResponse>
+                >({
+                    query: UPDATE_PAGE_MUTATION,
+                    variables: {
+                        id: page.id,
+                        data: payload
+                    },
+                    token
+                })
+                pageMutate()
+                return response
+            } catch (error: any) {
+                console.log("Errror updating page", error)
+                toast.error(error.message || "Error updating page")
+                return error
+            }
+        },
+        [token, page.id, pageMutate]
+    )
 
-	const handleUpdate = useCallback(
-		(editor?: Editor) => {
-			if (editor) {
-				// console.log("handleUpdate", editor.getHTML())
-				// console.log(editor.getJSON())
-				console.groupCollapsed("handleUpdate")
-				// console.log("draftBody", editor.getHTML())
-				console.log("jsonDraftBody", editor.getJSON())
-				console.groupEnd()
+    const handleUpdate = useCallback(
+        (editor?: Editor) => {
+            if (editor) {
+                // console.log("handleUpdate", editor.getHTML())
+                // console.log(editor.getJSON())
+                console.groupCollapsed("handleUpdate")
+                // console.log("draftBody", editor.getHTML())
+                console.log("jsonDraftBody", editor.getJSON())
+                console.groupEnd()
 
-				updatePage({
-					// draftBody: editor.getHTML(),
-					jsonDraftBody: editor.getJSON(),
-				})
-			}
-		},
-		[updatePage]
-	)
+                updatePage({
+                    // draftBody: editor.getHTML(),
+                    jsonDraftBody: editor.getJSON()
+                })
+            }
+        },
+        [updatePage]
+    )
 
-	const defaultValue = useCallback(() => {
-		if (page.jsonDraftBody) {
-			return page.jsonDraftBody as JSONContent
-		}
-		return page.draftBody as string
-	}, [page.jsonDraftBody, page.draftBody])
+    const defaultValue = useCallback(() => {
+        if (page.jsonDraftBody) {
+            return page.jsonDraftBody as JSONContent
+        }
+        return page.draftBody as string
+    }, [page.jsonDraftBody, page.draftBody])
 
-	return (
-		<motion.main
-			className={cn(
-				"min-h-screen",
-				isMinimized ? "w-full" : "w-full md:w-[calc(100%-20rem)]"
-			)}
-			initial={false}
-			animate={{
-				x: isMinimized ? 0 : "20rem",
-			}}
-			transition={{
-				bounce: 0,
-			}}>
-			<PageToolBar token={token} />
-			<NovelEditor
-				editorProps={{
-					attributes: {
-						pageId: page.id,
-						siteId,
-						pageSlug,
-						userId,
-						token,
-					},
-				}}
-				// token={token}
-				defaultValue={defaultValue()}
-				storageKey={`page-draftbody-${page.id}`}
-				disableLocalStorage
-				onDebouncedUpdate={value => handleUpdate(value)}
-				key={page.id}
-			/>
-		</motion.main>
-	)
+    return (
+        <motion.main
+            className={cn(
+                "min-h-screen",
+                isMinimized ? "w-full" : "w-full md:w-[calc(100%-20rem)]"
+            )}
+            initial={false}
+            animate={{
+                x: isMinimized ? 0 : "20rem"
+            }}
+            transition={{
+                bounce: 0
+            }}>
+            <PageToolBar token={token} />
+            <NovelEditor
+                editorProps={{
+                    attributes: {
+                        pageId: page.id,
+                        siteId,
+                        pageSlug,
+                        userId,
+                        token
+                    }
+                }}
+                // token={token}
+                defaultValue={defaultValue()}
+                storageKey={`page-draftbody-${page.id}`}
+                disableLocalStorage
+                onDebouncedUpdate={value => handleUpdate(value)}
+                key={page.id}
+            />
+        </motion.main>
+    )
 }
 
 export default PageView
--- apps/templates/hemingway/app/components/tiptap/template-extensions/button-node.tsx
+++ apps/templates/hemingway/app/components/tiptap/template-extensions/button-node.tsx
@@ -1,86 +1,90 @@
-import { cn } from "@/libs/cn"
+import { cn } from "@repo/utils/src/cn"
 import { Node, NodeViewProps } from "@tiptap/core"
-import { NodeViewContent, NodeViewWrapper, ReactNodeViewRenderer } from "@tiptap/react"
+import {
+    NodeViewContent,
+    NodeViewWrapper,
+    ReactNodeViewRenderer
+} from "@tiptap/react"
 import { useRouter } from "next/navigation"
 import Button from "../../Button"
 
 interface SetButtonOptions {
-	inline: boolean
-	href: string
-	target: string
-	variant: "primary" | "secondary" | "tertiary"
+    inline: boolean
+    href: string
+    target: string
+    variant: "primary" | "secondary" | "tertiary"
 }
 
 declare module "@tiptap/core" {
-	// eslint-disable-next-line
-	interface Commands<ReturnType> {
-		button: {
-			/**
-			 * Insert a Page node
-			 */
-			// eslint-disable-next-line no-unused-vars
-			setButton: (options: SetButtonOptions) => ReturnType
-		}
-	}
+    // eslint-disable-next-line
+    interface Commands<ReturnType> {
+        button: {
+            /**
+             * Insert a Page node
+             */
+            // eslint-disable-next-line no-unused-vars
+            setButton: (options: SetButtonOptions) => ReturnType
+        }
+    }
 }
 
 const ButtonNodeView = (props: NodeViewProps) => {
-	const { node } = props
-	const router = useRouter()
+    const { node } = props
+    const router = useRouter()
 
-	return (
-		<NodeViewWrapper>
-			<Button onClick={() => router.push(node.attrs.href)}>
-				{node.attrs.text}
-			</Button>
-		</NodeViewWrapper>
-	)
+    return (
+        <NodeViewWrapper>
+            <Button onClick={() => router.push(node.attrs.href)}>
+                {node.attrs.text}
+            </Button>
+        </NodeViewWrapper>
+    )
 }
 
 const ButtonNode = Node.create({
-	name: "button",
-	group: "inline",
-	inline: true,
-	atom: true,
-	content: "inline*",
-	isolating: true,
-	defining: true,
+    name: "button",
+    group: "inline",
+    inline: true,
+    atom: true,
+    content: "inline*",
+    isolating: true,
+    defining: true,
 
-	addAttributes() {
-		return {
-			href: {
-				default: null,
-			},
-			target: {
-				default: null,
-			},
-			rel: {
-				default: null,
-			},
-			text: {
-				default: "Click me!",
-			},
-			variant: {
-				default: "primary",
-			},
-		}
-	},
+    addAttributes() {
+        return {
+            href: {
+                default: null
+            },
+            target: {
+                default: null
+            },
+            rel: {
+                default: null
+            },
+            text: {
+                default: "Click me!"
+            },
+            variant: {
+                default: "primary"
+            }
+        }
+    },
 
-	parseHTML() {
-		return [
-			{
-				tag: "button",
-			},
-		]
-	},
+    parseHTML() {
+        return [
+            {
+                tag: "button"
+            }
+        ]
+    },
 
-	renderHTML({ HTMLAttributes }) {
-		return ["button", HTMLAttributes, 0]
-	},
+    renderHTML({ HTMLAttributes }) {
+        return ["button", HTMLAttributes, 0]
+    },
 
-	addNodeView() {
-		return ReactNodeViewRenderer(ButtonNodeView)
-	},
+    addNodeView() {
+        return ReactNodeViewRenderer(ButtonNodeView)
+    }
 })
 
 export default ButtonNode
